369d041d733186e7bd5be83e15276d5f58bf13ab
mouhamed92 <hamamimohamed92@gmail.com>
2023-06-04 11:31:48 +0200

    display seats

--- Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
@@ -4,5 +4,21 @@ public class Cinema {
 
     public static void main(String[] args) {
         // Write your code here
+        int i = 1 ;
+        System.out.println("Cinema:");
+        System.out.print(" ");
+        for (int c = 1 ; c <9 ; c++){
+           System.out.print(" "+c);
+        }
+        System.out.println();
+
+        do {
+            System.out.print(i);
+            for (int l = 1; l < 9; l++) {
+                System.out.print(" S");
+            }
+            System.out.println();
+            i++ ;
+        }while (i <8);
     }
 }
